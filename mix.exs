c029c603402e2bc5b25c5d9c881995858f268bf7
Justin Schneck <jschneck@mac.com>
2019-01-20 09:24:33 -0500

    v0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PBCS.MixProject do
   def project do
     [
       app: :pbcs,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.0",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
